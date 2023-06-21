f62be907d5d2a0b5cf3332abe76314fe6b8edaa4
Neko <lowpolyneko@protonmail.ch>
2023-06-20 20:54:34 -0500

    Add total movement calculation

--- Assets/Scripts/DirectionalGaze.cs
+++ Assets/Scripts/DirectionalGaze.cs
@@ -1,12 +1,14 @@
 using Microsoft.MixedReality.Toolkit;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class DirectionalGaze : MonoBehaviour
 {
 
     private Vector3 lastGazeDirection;
+    public Vector3 totalMovement;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,10 @@ public class DirectionalGaze : MonoBehaviour
     {
         // Calculate travel between two vectors
         Vector3 currentDir = CoreServices.InputSystem.GazeProvider.GazeDirection;
-        Debug.Log("Distance Traveled: " + (currentDir - lastGazeDirection));
+        Vector3 distanceTraveled = currentDir - lastGazeDirection;
+        totalMovement += distanceTraveled.Abs();
+        Debug.Log("Distance Traveled: " + distanceTraveled);
+        Debug.Log("Total Movement: " + totalMovement);
 
         lastGazeDirection = currentDir;
     }
