e66f6aec0a5b6f319ba23b6797b49a320709afd2
Neko <lowpolyneko@protonmail.ch>
2023-07-27 00:41:14 -0500

    Added SpeechHandler

---
+++ Assets/Scripts/SpeechHandler.cs
@@ -0,0 +1,40 @@
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechHandler : MonoBehaviour, IMixedRealitySpeechHandler
+{
+    public GameObject cubePrefab;
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        switch (eventData.Command.Keyword.ToLower())
+        {
+            case "cube": //spawn cube
+                SpawnCube();
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoreServices.InputSystem.RegisterHandler<IMixedRealitySpeechHandler>(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnCube()
+    {
+        if (!cubePrefab)
+            return;
+
+        Instantiate(cubePrefab, CoreServices.InputSystem.GazeProvider.GazeDirection, Quaternion.identity);
+    }
+}
