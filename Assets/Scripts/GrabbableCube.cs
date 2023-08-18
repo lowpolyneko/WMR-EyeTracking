4410f6032171957070b546dd62259913030ddd4e
Neko <lowpolyneko@protonmail.ch>
2023-08-18 03:33:13 -0500

    Add sound effects

--- Assets/Scripts/GrabbableCube.cs
+++ Assets/Scripts/GrabbableCube.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class GrabbableCube : MonoBehaviour
 {
     private Renderer myRenderer;
+    private AudioSource myAudioSource;
+
+    public AudioClip pickupSFX;
+    public AudioClip dropSFX;
 
     // Start is called before the first frame update
     void Start()
     {
         myRenderer = GetComponent<Renderer>();
+        myAudioSource = GetComponent<AudioSource>();
 
         // Set text on cube to be the name of the object
         GetComponentInChildren<TextMeshPro>().text = name;
@@ -25,10 +30,16 @@ public class GrabbableCube : MonoBehaviour
     public void OnGrabStart()
     {
         myRenderer.material.color = Color.green;
+
+        myAudioSource.clip = pickupSFX;
+        myAudioSource.Play();
     }
 
     public void OnGrabEnd()
     {
         myRenderer.material.color = Color.white;
+
+        myAudioSource.clip = dropSFX;
+        myAudioSource.Play();
     }
 }
