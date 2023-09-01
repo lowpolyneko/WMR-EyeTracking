ce11b4169e9c7a706b915e4163862775d2d1e4a6
Neko <lowpolyneko@protonmail.ch>
2023-08-31 23:44:37 -0500

    Always on gaze pointer

--- Assets/Scripts/SceneObject.cs
+++ Assets/Scripts/SceneObject.cs
@@ -1,3 +1,4 @@
+using Microsoft.MixedReality.Toolkit.Input;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,8 @@ public class SceneObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Always enable the gaze cursor
+        PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOn);
     }
 
     // Update is called once per frame
